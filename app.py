893db294023bd0a3c387095edbbe43839b53ff79
lelua <tauful01@myunitec.ac.nz>
2021-03-23 16:51:20 +1300

    changed gross

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+import PAYE
 
 app = Flask(__name__)
 CORS(app)
@@ -9,8 +10,15 @@ CORS(app)
 def hello_world():
     return "hello world"
 
+@app.route('/getNetIncome/', method=['POST'])
+def netIncome():
+    gross_income = request.get_json()
+    print(gross_income)
+    gross = float(gross_income["gross_income"])
+    return jsonify(gross - PAYE.totalTax(gross))
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
 
 
 
