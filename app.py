ca6ec85c4645b3ade69ae24b50fa7a7a86a95b2c
lelua <tauful01@myunitec.ac.nz>
2021-03-23 16:12:13 +1300

    init

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def hello_world():
+    return "hello world"
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
